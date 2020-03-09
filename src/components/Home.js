df51856a0078b833fdd739b9c158db6c8e256fd5
Manny Dheer <mannydheer@gmail.com>
2020-03-09 13:45:41 -0400

    COMPLETELY DONE

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ListingGrid from './ListingGrid';
 import styled from 'styled-components';
+import { Link } from "react-router-dom";
+
 
 
 const Home = (data) => {
@@ -22,12 +24,60 @@ const Home = (data) => {
         }) 
     }
     </StyledMainProductDiv>
+
+    <Footer>
+
+        <SocialMedia><Link to='#' className="fa fa-facebook"></Link></SocialMedia>
+        <SocialMedia><Link to='#' className="fa fa-twitter"></Link></SocialMedia>
+        <SocialMedia><Link to='#' className="fa fa-instagram"></Link></SocialMedia>
+    </Footer>
 </React.Fragment>)
 }
 
 export default Home;
 
 
+const Footer = styled.footer`
+height: 300px;
+color: black;
+background-image: linear-gradient(-20deg, #2b5876 0%, #4e4376 100%);display:flex;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+
+const SocialMedia = styled.span`
+padding: 30px;
+
+a {
+    text-decoration: none;
+}
+
+.fa-facebook{
+    font-size: 1.5em;
+
+    color: white;
+    &:hover {
+        color: pink;
+    }
+}
+.fa-twitter{
+    font-size: 1.5em;
+
+    color: white;
+    &:hover {
+        color: pink;
+    }
+}
+.fa-instagram{
+    font-size: 2.5em;
+
+    color: white;
+    &:hover {
+        color: pink;
+    }
+}
+`
 
 const StyledMainProductDiv = styled.div`
 background-image: linear-gradient(to top, #feada6 0%, #f5efef 100%);
@@ -35,10 +85,6 @@ display:inline-grid;
 grid-template-columns: repeat(3, 1fr);
 grid-gap: 100px 150px;
 padding: 50px;
-position: absolute;
-top: 30%;
-
-
 `
 
 const StyledFruitText = styled.div`
